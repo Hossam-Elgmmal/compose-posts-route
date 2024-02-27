c673f0116327cc277df476ce303bbf8aff309334
Hossam-Elgmmal <109397447+Hossam-Elgmmal@users.noreply.github.com>
2024-02-27 09:21:32 +0200

    refactored code

--- app/src/main/java/com/route/composefacebookposts/MainActivity.kt
+++ app/src/main/java/com/route/composefacebookposts/MainActivity.kt
@@ -11,8 +11,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -55,14 +58,13 @@ fun ItemPost(post: Post) {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        val (profImage, username, timeText, lang,
+        val (profImage, username, timeText, public,
             option, postText, postImage, likeCount, shareCount,
             like, comment, share) = createRefs()
 
-
-        Image(   // profile image
+        Image( // profile image
             painter = painterResource(id = R.drawable.spongebob),
-            contentDescription = "profilePicture",
+            contentDescription = stringResource(id = R.string.profile_description),
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(80.dp)
@@ -72,58 +74,69 @@ fun ItemPost(post: Post) {
                     start.linkTo(parent.start)
                 }
         )
-        Text(  // username
-            text = "SpongeBob",
-            color = Color.Black,
+        Text( // username
+            text = stringResource(id = R.string.username),
+            style = MaterialTheme.typography.titleMedium,
             modifier = Modifier
                 .constrainAs(username) {
                     start.linkTo(profImage.end, margin = 16.dp)
                     top.linkTo(profImage.top, margin = 16.dp)
                 }
         )
-        Text(  //time
+        Text( // time
             text = stringResource(id = post.timeId),
-            color = Color.DarkGray,
+            style = MaterialTheme.typography.labelMedium,
             modifier = Modifier.constrainAs(timeText) {
                 top.linkTo(username.bottom, margin = 8.dp)
                 start.linkTo(username.start)
             }
         )
-
-        Icon(painter = painterResource(id = R.drawable.ic_earth), contentDescription = "language",
+        Icon( // public icon
+            painter = painterResource(id = R.drawable.ic_public),
+            contentDescription = stringResource(id = R.string.public_icon),
             tint = Color.DarkGray,
             modifier = Modifier
                 .size(16.dp)
-                .constrainAs(lang) {
+                .constrainAs(public) {
                     top.linkTo(timeText.top)
                     start.linkTo(timeText.end, margin = 8.dp)
                     bottom.linkTo(timeText.bottom)
                 }
-
         )
-        Icon(painter = painterResource(id = R.drawable.ic_options), contentDescription = "option",
-            tint = Color.DarkGray,
+        IconButton( // options
+            onClick = {},
+            colors = IconButtonDefaults
+                .iconButtonColors(
+                    Color.Transparent,
+                    Color.DarkGray,
+                    Color.Gray,
+                    Color.DarkGray
+                ),
             modifier = Modifier
                 .size(24.dp)
                 .constrainAs(option) {
                     end.linkTo(parent.end)
                     top.linkTo(username.top)
                     bottom.linkTo(username.bottom)
-                }
-        )
-
+                }) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_options),
+                contentDescription = stringResource(id = R.string.options)
+            )
+        }
         Text(  // post text
             text = stringResource(id = post.textId),
+            style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier
                 .constrainAs(postText) {
                     top.linkTo(profImage.bottom, margin = 16.dp)
                     start.linkTo(profImage.start)
                 }
         )
-        Image(painter = painterResource(id = post.imageId),
+        Image( // post image
+            painter = painterResource(id = post.imageId),
             contentScale = ContentScale.FillBounds,
-
-            contentDescription = "post image",
+            contentDescription = stringResource(id = R.string.post_image),
             modifier = Modifier
                 .fillMaxSize()
                 .aspectRatio(1f)
@@ -133,28 +146,18 @@ fun ItemPost(post: Post) {
                     end.linkTo(parent.end)
                 }
         )
-        TextButton(onClick = {},
-            colors = ButtonDefaults
-                .buttonColors(
-                    Color.Transparent,
-                    Color.DarkGray,
-                    Color.Gray,
-                    Color.DarkGray
-                ),
-            modifier = Modifier
-                .constrainAs(likeCount) {
-                    top.linkTo(postImage.bottom)
-                    start.linkTo(parent.start)
-                }
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_small_like),
-                contentDescription = "like count", tint = Color(1, 102, 226)
-            )
-            Spacer(modifier = Modifier.padding(2.dp))
-            Text(text = "112")
-        }
-        TextButton(onClick = {},
+        CustomTextButton( // likes count
+            R.string.likes_count, R.drawable.ic_small_like,
+            Modifier.constrainAs(likeCount)
+            {
+                top.linkTo(postImage.bottom)
+                start.linkTo(parent.start)
+            },
+            Color(1, 102, 226),
+            R.string.likes_description
+        ) {}
+        TextButton( // shares count
+            onClick = {},
             colors = ButtonDefaults
                 .buttonColors(
                     Color.Transparent,
@@ -170,7 +173,6 @@ fun ItemPost(post: Post) {
         ) {
             Text(text = "Share")
             Spacer(modifier = Modifier.padding(2.dp))
-
             Image(
                 painter = painterResource(id = R.drawable.spongebob),
                 contentDescription = "profilePicture",
@@ -178,7 +180,6 @@ fun ItemPost(post: Post) {
                 modifier = Modifier
                     .size(30.dp)
                     .clip(CircleShape)
-
             )
             Spacer(modifier = Modifier.padding(2.dp))
             Icon(
@@ -186,80 +187,58 @@ fun ItemPost(post: Post) {
                 contentDescription = "drop down"
             )
         }
-        TextButton(onClick = {},
-            colors = ButtonDefaults
-                .buttonColors(
-                    Color.Transparent,
-                    Color.DarkGray,
-                    Color.Gray,
-                    Color.DarkGray
-                ),
-
-            modifier = Modifier
+        CustomTextButton( // like
+            R.string.like, R.drawable.ic_like, Modifier
                 .constrainAs(like) {
                     start.linkTo(parent.start)
                     top.linkTo(likeCount.bottom)
                     end.linkTo(comment.start)
-                }
-
-        ) {
-
-            Icon(painter = painterResource(id = R.drawable.ic_like), contentDescription = "like")
-            Spacer(modifier = Modifier.padding(4.dp))
-            Text(text = "Like")
-        }
-        TextButton(onClick = {},
-            colors = ButtonDefaults
-                .buttonColors(
-                    Color.Transparent,
-                    Color.DarkGray,
-                    Color.Gray,
-                    Color.DarkGray
-                ),
-
-            modifier = Modifier
+                }) {}
+        CustomTextButton( // comment
+            R.string.comment, R.drawable.ic_comment, Modifier
                 .constrainAs(comment) {
                     top.linkTo(like.top)
                     start.linkTo(like.end)
                     end.linkTo(share.start)
-                }
-
-
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_comment),
-                contentDescription = "comment"
-            )
-            Spacer(modifier = Modifier.padding(4.dp))
-            Text(text = "Comment")
-
-        }
-        TextButton(onClick = {},
-            colors = ButtonDefaults
-                .buttonColors(
-                    Color.Transparent,
-                    Color.DarkGray,
-                    Color.Gray,
-                    Color.DarkGray
-                ),
-
-            modifier = Modifier
+                }) {}
+        CustomTextButton( // share
+            R.string.share, R.drawable.ic_share, Modifier
                 .constrainAs(share) {
                     top.linkTo(like.top)
                     end.linkTo(parent.end)
                     start.linkTo(comment.end)
-                }
+                }) {}
+    }
+}
 
-        ) {
-            Icon(painter = painterResource(id = R.drawable.ic_share), contentDescription = "share")
-            Spacer(modifier = Modifier.padding(4.dp))
-            Text(text = "Share")
-
-        }
-
+@Composable
+fun CustomTextButton(
+    textId: Int, iconId: Int, modifier: Modifier,
+    iconTint: Color = Color.DarkGray,
+    descriptionId: Int = textId, onClick: () -> Unit
+) {
+    TextButton(
+        onClick = onClick,
+        colors = ButtonDefaults
+            .buttonColors(
+                Color.Transparent,
+                Color.DarkGray,
+                Color.Gray,
+                Color.DarkGray
+            ),
+        shape = RoundedCornerShape(15.dp),
+        modifier = modifier
+    )
+    {
+        Icon(
+            painter = painterResource(id = iconId),
+            contentDescription = stringResource(id = descriptionId),
+            tint = iconTint
+        )
+        Spacer(modifier = Modifier.padding(4.dp))
+        Text(text = stringResource(id = textId))
 
     }
-
 }
 
 @Composable
